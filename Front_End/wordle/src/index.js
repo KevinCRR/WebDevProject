98efdf54ad154387ba48a0564ef31d25165213a3
KevinCRR <47986882+KevinCRR@users.noreply.github.com>
2022-04-23 23:17:43 -0400

    Navigation Bar
    create the main bones of the navigation bar functionality, still need to add mobile styling, and page switch before starting work on the other pages.

--- Front_End/wordle/src/index.js
+++ Front_End/wordle/src/index.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Scores from "./scores";
+import Navigation from "./header";
+import reportWebVitals from "./reportWebVitals";
+import $ from "jquery";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+var page = "main";
+$(function () {
+  $(".Menu").on("click", function (e) {
+    e.preventDefault();
+    if ($(".side-categories").is(":visible")) {
+      $(".side-categories").hide();
+    } else {
+      $(".side-categories").show();
+    }
+  });
+
+  $(".Scoreboard").on("click", function (e) {
+    page = "scoreboard";
+    console.log("clicked");
+  });
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Navigation />
+    <ul className="side-categories" style={{ display: "none" }}>
+      <li>
+        <a href="#">Login</a>
+      </li>
+      <hr />
+      <li>
+        <a href="#">Register</a>
+      </li>
+    </ul>
+
+    {page === "scoreboard" ? <Scores /> : <App />}
   </React.StrictMode>
 );
 
---
+++ Front_End/wordle/src/scores.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import React from "react";
+
+function scores() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Here will go the d3 scores</p>
+      </header>
+    </div>
+  );
+}
+
+export default scores;
