8cb6f11bf1c09dfe339d0a0dfed7f36988afd12f
PeterRegas <55000513+PeterRegas@users.noreply.github.com>
2022-04-25 17:26:10 -0400

    created login and register for frontend
    creation not working yet

---
+++ Front_End/wordle/src/login.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import React from "react";
+
+function login() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Here will go the login</p>
+      </header>
+    </div>
+  );
+}
+
+export default login;
