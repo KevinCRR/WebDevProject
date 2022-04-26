cc853d0c9de98a874ba77408b91a3e60fd09b7dd
KevinCRR <47986882+KevinCRR@users.noreply.github.com>
2022-04-26 14:09:40 -0400

    Added login to Peter's changes
    Login is currently in test mode.

--- Front_End/wordle/src/index.js
+++ Front_End/wordle/src/index.js
@@ -2,24 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./Styles/index.css";
 import App from "../src/Views/App";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-   <App />
-    <Navigation />
+    <App />
     <ul className="side-categories" style={{ display: "none" }}>
-      <li>
-        <a href="login">Login</a>
-      </li>
-      <hr />
-      <li>
-        <a href="register">Register</a>
-      </li>
+      <li></li>
     </ul>
-
-    {page === "scoreboard" ? <Scores /> : <App />}
   </React.StrictMode>
 );
-
