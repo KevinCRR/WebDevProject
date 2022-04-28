19ef99e616955b4661c6433b043d1c43ce8dc07a
PaulKatigbak <katigbakpaul@gmail.com>
2022-04-27 18:43:46 -0600

    Updated Board

--- Front_End/wordle/src/Views/BoardView.js
+++ Front_End/wordle/src/Views/BoardView.js
@@ -2,11 +2,20 @@ import React from 'react'
 import GameBoard from '../Components/Board/GameBoard'
 
 function BoardView() {
-  return (
-    <div>
-      <GameBoard />
-    </div>
-  )
+  var userInfo = localStorage.getItem("userInfo");
+
+  if (userInfo) {
+    return (
+      <div>
+        <GameBoard />
+      </div>
+    );
+  }else{
+    return<div className='register'>Please Login to Start Playing!!</div>;
+  }
+
+  localStorage.removeItem("userInfo");
+  return <div className="register">Logged Out! Login to View Game Board </div>;
 }
 
 export default BoardView
