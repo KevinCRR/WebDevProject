023bd19c55c0d0aa3b1ee59fa8d5b74ce4d80eea
KevinCRR <47986882+KevinCRR@users.noreply.github.com>
2022-04-27 20:58:06 -0400

    Login and Profile View fixes

--- Front_End/wordle/src/Views/LoginView.js
+++ Front_End/wordle/src/Views/LoginView.js
@@ -5,140 +5,157 @@ import axios from "axios";
 import "../Styles/login.css";
 import { useNavigate } from "react-router-dom";
 
-function LoginView({ setToken }) {
+function LoginView() {
   // states for error handling and login flags
   const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const [userName, setuserName] = useState("User");
+  const [isSignedIn, setIsSignedIn] = useState("Sign In");
+  const [userName, setuserName] = useState();
   let navigate = useNavigate();
-  // const [isLoggedIn, setLoggedFlag] = useState(false);
-  var username = "User";
+  var userInfo = localStorage.getItem("userInfo");
 
-  const errors = {
-    uname: "invalid username!",
-    pass: "invalid password",
-  };
+  if (!userInfo) {
+    // const [isLoggedIn, setLoggedFlag] = useState(false);
+    var username = "User";
 
-  // place holder for db will change to match api call
-  const database = [
-    {
-      username: "peter",
-      password: "Peter1",
-    },
-    {
-      username: "billy",
-      password: "Test1",
-    },
-  ];
-
-  const delay = (millis) =>
-    new Promise((resolve, reject) => {
-      setTimeout((_) => resolve(), millis);
-    });
-
-  const redirect = async () => {
-    console.log("here");
-    await delay(10000);
-    navigate("/");
-  };
-
-  // this will become an axios call
-  const handleSubmit = (event) => {
-    //Prevent page reload
-    event.preventDefault();
-    console.log(document.forms[0]);
-    var { uname, pass } = document.forms[0];
-    console.log(uname.value);
-    console.log(pass);
-
-    var config = {
-      method: "get",
-      url: "http://127.0.0.1:5000/login/" + uname.value + "/" + pass.value,
-      headers: {},
+    const errors = {
+      uname: "invalid username!",
+      pass: "invalid password",
     };
 
-    axios(config)
-      .then(function (response) {
-        var data = JSON.stringify(response.data);
-        if (data.includes("[")) {
-          localStorage.setItem("userInfo", data);
-          data = JSON.parse(data);
-          setuserName(data[0].username);
-          console.log(data);
-          // setter
-        } else {
-        }
-        setIsSubmitted(true);
-      })
-      .catch(function (error) {
-        console.log(error);
+    // place holder for db will change to match api call
+    const database = [
+      {
+        username: "peter",
+        password: "Peter1",
+      },
+      {
+        username: "billy",
+        password: "Test1",
+      },
+    ];
+
+    const delay = (millis) =>
+      new Promise((resolve, reject) => {
+        setTimeout((_) => resolve(), millis);
       });
 
-    // Will change to call backend api, right not just a place holder.
-    const userData = database.find((user) => user.username === uname.value);
+    const redirect = async () => {
+      console.log("here");
+      await delay(10000);
+      navigate("/");
+    };
 
-    // Compare user info (this will be moved to backedn)
+    // this will become an axios call
+    const handleSubmit = (event) => {
+      setIsSubmitted(true);
+      //Prevent page reload
+      event.preventDefault();
+      console.log(document.forms[0]);
+      var { uname, pass } = document.forms[0];
+      console.log(uname.value);
+      console.log(pass);
 
-    if (userData) {
-      if (userData.password !== pass.value) {
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        // setLoggedFlag(true);
-        setToken(true);
-      }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
-    }
-  };
+      var config = {
+        method: "get",
+        url: "http://127.0.0.1:5000/login/" + uname.value + "/" + pass.value,
+        headers: {},
+      };
 
-  // Error code jsx messahe
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
+      axios(config)
+        .then(function (response) {
+          var data = JSON.stringify(response.data);
+          if (data.includes("[")) {
+            localStorage.setItem("userInfo", data);
+            data = JSON.parse(data);
+            setuserName(data[0].username);
+            // setter
+          } else {
+            localStorage.setItem("userInfo", uname.value);
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+      // // Will change to call backend api, right not just a place holder.
+      // const userData = database.find((user) => user.username === uname.value);
+
+      // // Compare user info (this will be moved to backedn)
+
+      // if (userData) {
+      //   if (userData.password !== pass.value) {
+      //     setErrorMessages({ name: "pass", message: errors.pass });
+      //   } else {
+      //     // setLoggedFlag(true);
+      //     setToken(true);
+      //   }
+      // } else {
+      //   // Username not found
+      //   setErrorMessages({ name: "uname", message: errors.uname });
+      // }
+    };
+
+    // Error code jsx messahe
+    const renderErrorMessage = (name) =>
+      name === errorMessages.name && (
+        <div className="error">{errorMessages.message}</div>
+      );
+
+    const renderLoading = <div>Loading Please wait..</div>;
+    // Form login
+    const renderForm = (
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            name="uname"
+            placeholder="Enter username"
+            required
+          />
+          {renderErrorMessage("uname")}
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="pass"
+            placeholder="Password"
+            required
+          />
+          {renderErrorMessage("pass")}
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     );
 
-  // Form login
-  const renderForm = (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Username</Form.Label>
-        <Form.Control
-          type="text"
-          name="uname"
-          placeholder="Enter username"
-          required
-        />
-        {renderErrorMessage("uname")}
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          name="pass"
-          placeholder="Password"
-          required
-        />
-        {renderErrorMessage("pass")}
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  );
-
+    return (
+      <div className="app">
+        <div className="login-form">
+          <div className="sign-title">{isSignedIn}</div>
+          {isSubmitted ? (
+            userName ? (
+              <div>
+                {userName} is successfully logged in {redirect}
+              </div>
+            ) : (
+              renderLoading
+            )
+          ) : (
+            renderForm
+          )}
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="app">
       <div className="login-form">
-        <div className="sign-title">Sign In</div>
-        {isSubmitted ? (
-          <div>
-            {userName} is successfully logged in {redirect}
-          </div>
-        ) : (
-          renderForm
-        )}
+        <div>Logged in</div>
       </div>
     </div>
   );
--- Front_End/wordle/src/Views/ProfileView.js
+++ Front_End/wordle/src/Views/ProfileView.js
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Card from "react-bootstrap/Card";
 
 function ProfileView() {
+  const [scores, setScores] = useState();
   var userInfo = localStorage.getItem("userInfo");
+  useEffect(() => {
+    if (userInfo) {
+      async function fetchData() {
+        try {
+          const response = await axios.get(
+            "http://127.0.0.1:5000/scoreUser/" + { userInfo }
+          );
+          setScores(JSON.parse(response.data));
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      fetchData();
+    } else {
+    }
+  }, []);
 
   if (userInfo) {
-    userInfo = JSON.parse(userInfo);
-
-    const WelcomeMessage = userInfo.map((score) => (
-      <Card border="primary" style={{ width: "18rem" }}>
-        {console.log(score)}
-        <Card.Header>Date: {score.date}</Card.Header>
-        <Card.Body>
-          <Card.Title>Score: {score.score}</Card.Title>
-        </Card.Body>
-      </Card>
-    ));
+    var WelcomeMessage = "";
+    if (scores) {
+      WelcomeMessage = scores.map((score) => (
+        <Card border="primary" style={{ width: "18rem" }}>
+          {console.log(score)}
+          <Card.Header>Date: {score.date}</Card.Header>
+          <Card.Body>
+            <Card.Title>Score: {score.score}</Card.Title>
+          </Card.Body>
+        </Card>
+      ));
+    } else {
+      WelcomeMessage = (
+        <Card border="primary" style={{ width: "18rem" }}>
+          <Card.Body>You don't have any scores</Card.Body>
+        </Card>
+      );
+    }
 
     return (
       <div className="register">
-        User:{userInfo[0].username}
+        User:{userInfo}
         {WelcomeMessage}
       </div>
     );
