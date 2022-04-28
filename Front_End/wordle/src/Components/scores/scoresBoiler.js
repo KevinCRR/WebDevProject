24e05ecf08ff5801a321447a916dd0f014b3eddc
PeterRegas <55000513+PeterRegas@users.noreply.github.com>
2022-04-27 20:51:30 -0400

    score board update and minor server changes to pull 20 instead of 10

--- Front_End/wordle/src/Components/scores/scoresBoiler.js
+++ Front_End/wordle/src/Components/scores/scoresBoiler.js
@@ -8,7 +8,7 @@ function BarChart({ data }) {
   const ref = useD3(
     (svg) => {
       const height = 500;
-      const width = 500;
+      const width = 950;
       const margin = { top: 20, right: 30, bottom: 30, left: 40 };
 
       const x = d3
